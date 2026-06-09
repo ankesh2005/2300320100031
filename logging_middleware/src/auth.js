b6e1441810c25606fed7eafff113fb388d6f30fe
ankesh2005 <ankeshgupta1802@gmail.com>
2026-06-09 11:46:11 +0530

    create gettoken function to fetch token from server

---
+++ logging_middleware/src/auth.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+require("dotenv").config();
+
+let token = null;
+
+async function getToken() {
+  if (token) return token;
+
+  const response = await axios.post(
+    "http://4.224.186.213/evaluation-service/auth",
+    {
+      email: process.env.EMAIL,
+      name: process.env.NAME,
+      rollNo: process.env.ROLL_NO,
+      accessCode: process.env.ACCESS_CODE,
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    }
+  );
+
+  token = response.data.access_token;
+
+  return token;
+}
+
+module.exports = { getToken };
